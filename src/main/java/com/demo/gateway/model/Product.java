6380d9973be887fea2eb5672a45dc676e464c7b1
Lê Tấn Nguyên <letannguyen@MacBook-Air-cua-Le-2.local>
2023-01-11 10:24:25 +0700

    eureka and elasticsearch

---
+++ src/main/java/com/demo/gateway/model/Product.java
@@ -0,0 +1,30 @@
+package com.demo.gateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(indexName = "products")
+public class Product {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text, name = "name")
+    private String name;
+
+    @Field(type = FieldType.Text, name = "description")
+    private String description;
+
+    @Field(type = FieldType.Double, name = "price")
+    private double price;
+}
